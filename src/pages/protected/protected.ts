6db3f0f318ba468d338d7446d27320f20a05f9a7
Howard Ng <ngkung@gmail.com>
2018-03-13 17:11:01 +0800

    not a word

---
+++ src/pages/protected/protected.ts
@@ -0,0 +1,31 @@
+import { NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+/**
+ * Generated class for the ProtectedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+export class ProtectedPage {
+
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public storage: Storage) {
+    }
+
+  ionViewDidLoad() {
+      this.storage.get('id_token').then(id_token => {
+          if (id_token === null) {
+              this.navCtrl.setRoot('LoginPage');
+              return false;
+          }
+      });
+      return;
+  }
+
+
+
+}
