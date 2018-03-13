abbff4d47b8e4359ffabc2829c4728ba6d2085ce
Howard Ng <ngkung@gmail.com>
2018-03-13 17:05:22 +0800

    not a word

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { TranslateService } from '@ngx-translate/core';
-import {AuthService} from '../providers/authService';
+import {AuthService} from '../providers/authService.ts';
 
 @Component({
   templateUrl: 'app.html'
